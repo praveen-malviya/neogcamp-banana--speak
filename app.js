8e8ec8adca3fcdd070efbfa4f0f713f517c437d4
praveen-malviya <contact.praveenmalviya@gmail.com>
2021-04-14 23:05:45 +0530

    Feat: Output wired
    We have created a div to show Output .. based on input

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var userInput = document.querySelector("#userInput");
-console.log(btnTranslate);
+var output = document.querySelector("#outputText");
 
 function clickHandler(){
     // console.log("clicked");
-    console.log(userInput.value);
+    // console.log(userInput.value);
+    output.innerHTML="you typed "+ userInput.value;
 };
 
 btnTranslate.addEventListener("click", clickHandler);

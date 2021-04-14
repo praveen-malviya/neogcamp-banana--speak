78968b3247d55d1ce0ab61c17ed4de59dbadb1ec
praveen-malviya <contact.praveenmalviya@gmail.com>
2021-04-15 02:09:55 +0530

    Feat: Add Real server
    added Real API server from funtranslation.

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ var btnTranslate = document.querySelector("#btn-translate");
 var userInput = document.querySelector("#userInput");
 var outputDiv = document.querySelector("#outputText");
 
-var serverURL = "https://serverapi.praveenmalviya.repl.co/translate/pm.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-// console.log(serverURL);
-
-// var callingapi = serverURL+"?text="+userInput.value;
+function errorHandler(error) {
+    console.log("Error occured", error);
+    alert("something went wrong with server! Try after 1 hour")
+}
 
 
 
 
 function clickHandler(){
-    var callingapi = serverURL+"?text="+userInput.value;
+    var callingapi = serverURL+"?"+"text="+userInput.value;
     fetch(callingapi)
     .then(response => response.json())
     .then(json => {
         var translatedText = json.contents.translated;
         outputDiv.innerText = translatedText;
     })
+    .catch(errorHandler)
     
 };
 
